f740d5db2f508c8630525646d62cfec93935ebc1
JRGardnerSN <u156923067@byui.edu>
2023-02-08 13:03:07 -0800

    working on week03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.Clear();
+        UserInterface.startUser();
+
+
+
+        // This will start by displaying "AAA" and waiting for the user to press the enter key
+        Console.WriteLine("AAA");
+        Console.ReadLine();
+
+        // This will clear the console
+        Console.Clear();
+
+        // This will show "BBB" in the console where "AAA" used to be
+        Console.WriteLine("BBB");
     }
 }
---
+++ prove/Develop03/User.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class User {
+        //►
+    string _username = "";
+    string _password = "";
+    //List<Scripture> _favoriteSrciptures = new List<Scripture>();
+}
+
+public class UserInterface {
+
+    public static void startUser() {
+        Console.WriteLine("Please select an action:");
+        Console.WriteLine();
+        Console.WriteLine("1. Create new user");
+        Console.WriteLine("2. Load existing user");
+        // Console.WriteLine("3. ");
+        string numberSelect = Console.ReadLine();
+        if (numberSelect == "1") {
+            Console.Clear();
+            Console.WriteLine("Username: ");
+            string newUsername = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            string newPassword = Console.ReadLine();
+            
+        }
+    }
+}
